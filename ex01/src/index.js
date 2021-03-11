7c19d8ff8ecb81543bb679c471cd41bf7c3f3c51
EP-84 <edmir.prasovic@gmail.com>
2021-03-11 23:31:21 +0100

    Postavljanje uradjenih vjezbi

---
+++ ex01/src/index.js
@@ -0,0 +1,26 @@
+function main(numOne, numTwo) { 
+
+    var quotient;
+    if(numTwo === 0){
+        quotient = 'Davide by 0! Cannot divide by yero!';
+    } else { 
+        // Only change code below this line
+
+        var numOne;
+        var numTwo;
+        var quotient; 
+
+        numOne = 15;
+        numTwo = 3;
+        quotient = numOne / numTwo;
+
+
+        // Only change code above this line
+
+     } 
+
+return quotient;
+}
+
+console.log(main(15, 3)); // change this line
+module.exports = main
